1c3aaeb4823bc39109ee7c7a1ec8267192de9bba
codeeits <68823929+codeeits@users.noreply.github.com>
2025-04-01 11:01:47 +0300

    Update CntImpare.cpp

--- CntImpare.cpp
+++ CntImpare.cpp
@@ -1,1 +1,30 @@
+//
+//  main.cpp
+//  CntImpare
+//
+//  Created by Robert Ciobanu on 01.04.2025.
+//
 
+#include <iostream>
+using namespace std;
+
+int CntImpare(int a[], int st, int dr){
+    if(st==dr){
+        return a[st]%2;
+    }
+    
+    int mij = (st+dr)/2;
+    int s1 = CntImpare(a, st, mij);
+    int s2 = CntImpare(a, mij+1, dr);
+    
+    return s1+s2;
+}
+
+int main() {
+    int n, a[1000];
+    cin >> n;
+    for(int i = 0; i<n; i++){
+        cin >> a[i];
+    }
+    cout << CntImpare(a, 0, n-1);
+}
